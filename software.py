45b13ae55af4934eea1b4b7f2d409162300e1639
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2024-08-02 13:12:09 -0700

    software.py settings
    banning users, and approved list

---
+++ software.py
@@ -0,0 +1,11 @@
+# Type software names that are okay to be inputted into. Make sure to include the .exe extension.
+# For example, notepad will be "notepad.exe" in the list
+# If not sure what the process name is, check Task Manager
+# For your safety, only include software you intend for users to interact with.
+ApprovedExeProcesses = []
+
+# Type user names that should not be allowed to input commands
+# Useful for banning bots or trolls from inputting commands, either by accident or on purpose
+BannedUsers = []
+
+UseMode = 1 # 0 for false, 1 for true
