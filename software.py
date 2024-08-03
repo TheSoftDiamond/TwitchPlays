19da39ef2548f7c1f76dc26c5ef2e565b0bb3c6e
SoftDiamond <30062102+TheSoftDiamond@users.noreply.github.com>
2024-08-02 23:11:35 -0700

    Update software.py

--- software.py
+++ software.py
@@ -9,3 +9,8 @@ ApprovedExeProcesses = []
 BannedUsers = []
 
 UseMode = 1 # 0 for false, 1 for true
+
+# Enter as many names as you wish to here, as these are essentially different profiles for your Twitch Plays, so you can jump between different games or software.
+# This will require you to also program in the logic in which games or software reacts to the chat commands in the main.py file yourself. The names must match the names in this list.
+# I recommend adding the .lower() function to the names in the list, so that the names are not case sensitive.
+SoftwareFormats = []
